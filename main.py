e60defbc246f7f07fd29392c93e957466b6c30dc
Kaiyu Zheng <kaiyutony@gmail.com>
2021-03-17 21:38:03 -0400

    experiment and plot

--- main.py
+++ main.py
@@ -31,7 +31,8 @@ def estimate_pi_multi_proc(n, nproc=4):
 
 def plot_estimates():
     # Plot the estimate vs number of samples
-    nsamples = [100, 1e3, 1e4]#10, 30, 50] #80, 100, 200, 500, 700, 1e3, 2e3, 5e3, 7e3, 1e4]
+    #10, 30, 50] #80, 100, 200, 500, 700, 1e3, 2e3, 5e3, 7e3, 1e4]
+    nsamples = [100, 300, 500, 700, 1e3, 1e4, 3e4, 5e4, 7e4, 1e5, 3e5, 5e5, 7e5, 1e6]
     estimates = []
     for ns in nsamples:
 
@@ -50,7 +51,7 @@ def plot_estimates():
             estimates.append(["single", ns, est_pi, time_est_pi])
 
             _start = time.time()
-            est_pi_mp = estimate_pi_multi_proc(int(ns), nproc=4)
+            est_pi_mp = estimate_pi_multi_proc(int(ns), nproc=8)
             time_est_pi_mp = time.time() - _start
             estimates.append(["multi", ns, est_pi_mp, time_est_pi_mp])
 
@@ -58,13 +59,14 @@ def plot_estimates():
 
     df = pandas.DataFrame(estimates, columns=["type", "num_samples", "est_pi", "time"])
 
-    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10,5))
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(18,9))
     sns.lineplot(x="num_samples",
                  y="est_pi",
                  hue="type",
                  data=df,
                  ax=axes[0])
     axes[0].axhline(y=math.pi, color='k', linestyle="--")
+    axes[0].set_ylim(3.12, 3.16)
 
     sns.lineplot(x="num_samples",
                  y="time",
