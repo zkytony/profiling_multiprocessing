29f024375092ebff7aee314b873ed81fe4cb9277
Kaiyu Zheng <kaiyutony@gmail.com>
2021-03-17 23:18:56 -0400

    simplify

--- estimate_pi.py
+++ estimate_pi.py
@@ -4,14 +4,14 @@ import math
 def estimate_pi(n):
     """Monte carlo estimation of pi; based on
     ratio between area in quarter circle vs unit square"""
-    points_out = []
-    points_in = []
+    points_out = 0
+    points_in = 0
     for i in range(n):
         x = random.uniform(0,1)
         y = random.uniform(0,1)
         if math.sqrt(x**2 + y**2) <= 1:
-            points_in.append((x,y))
+            points_in += 1
         else:
-            points_out.append((x,y))
-    est_pi = (len(points_in) / (len(points_out) + len(points_in))) * 4
+            points_out += 1
+    est_pi = (points_in / (points_out + points_in)) * 4
     return est_pi
